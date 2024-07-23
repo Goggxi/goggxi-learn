64e3acb0fa465a4ebc36edaea01e8bf27bd6d0c9
Goggxi <goggxi@gmail.com>
2024-07-23 21:46:58 +0700

    improve response album

--- Python/django-songs-api/api/views/album.py
+++ Python/django-songs-api/api/views/album.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from api.models import Album
 from api.serializers import AlbumSerializer
 
@@ -6,6 +7,20 @@ from api.serializers import AlbumSerializer
 class AlbumListCreateAPIView(generics.ListCreateAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        return Response({
+            'message': 'Albums retrieved successfully',
+            'data': response.data
+        })
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response({
+            'message': 'Album created successfully',
+            'data': response.data
+        })
 
 
 class AlbumRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
